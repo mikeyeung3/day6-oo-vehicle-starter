03849eb2318263483f2459507d976a08b9c7e37e
Mike Yeung <mikeyeung316@gmail.com>
2025-09-08 16:47:58 +0800

    [Story 1] fix: parking logic on position finding.

--- src/main/java/com/afs/ParkingLot.java
+++ src/main/java/com/afs/ParkingLot.java
@@ -3,11 +3,13 @@ package com.afs;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 public class ParkingLot {
     private final Integer id;
     private final HashMap<ParkingTicket, Car> parkingMap = new HashMap<>();
     private final Set<Car> cars = new HashSet<>();
+    private final Set<Integer> occupiedPositions = new HashSet<>();
     private static final Integer capacity = 10;
 
     public ParkingLot(int id) {
@@ -18,10 +20,25 @@ public class ParkingLot {
         if (cars.contains(car) || cars.size() >= capacity) {
             return null;
         }
-        ParkingTicket parkingTicket = new ParkingTicket(car, cars.size() + 1, this);
+
+        Integer position = findNextAvailablePosition();
+        if (position == null) {
+            return null;
+        }
+
+        ParkingTicket parkingTicket = new ParkingTicket(car, position, this);
         parkingMap.put(parkingTicket, car);
         cars.add(car);
+        occupiedPositions.add(position);
         return parkingTicket;
+    }
+
+    private Integer findNextAvailablePosition() {
+        return IntStream.rangeClosed(1, capacity)
+                .filter(i -> !occupiedPositions.contains(i))
+                .boxed()
+                .findFirst()
+                .orElse(null);
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
@@ -29,6 +46,7 @@ public class ParkingLot {
             Car car = parkingMap.get(parkingTicket);
             parkingMap.remove(parkingTicket);
             cars.remove(car);
+            occupiedPositions.remove(parkingTicket.position());
             return car;
         }
         return null;
